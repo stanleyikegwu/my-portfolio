33629fdc7a8310a862098649b1bc57a7a1fea7d0
stanley ikegwu <stagasstanley@gmail.com>
2025-08-12 15:55:22 +0100

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,9 +4,31 @@ const Projects = () => {
   return (
     <div>
       <h3>Projects</h3>
-      <p><li>smarthydro bottle order website</li>
-          <li>Hotel booking website</li>
-          <li>Resurant menu website</li> </p>
+      <ul>
+        <li>SmartHydro Bottle Order Website</li>
+        <li>Hotel Booking Website</li>
+        <li>Restaurant Menu Website</li>
+        <li>
+          My Portfolio App —{" "}
+          <a
+            href="https://cerulean-jelly-9fb282.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Link
+          </a>
+        </li>
+        <li>
+          Weather App —{" "}
+          <a
+            href="https://musical-hotteok-5fea59.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Link
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
